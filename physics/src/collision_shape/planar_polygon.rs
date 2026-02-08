39b6c106f8b028fef67bb7d568409f254a3ece60
Srinu427 <srinivasadigopula@gmail.com>
2026-02-08 22:20:18 +0530

    track separation plane by face or edge ids

--- physics/src/collision_shape/planar_polygon.rs
+++ physics/src/collision_shape/planar_polygon.rs
@@ -4,15 +4,16 @@ use glam::Vec4Swizzles;
 pub struct PlanarPolygon {
     pub(crate) pl: glam::Vec4,
     pub(crate) points: Vec<glam::Vec4>,
+    pub(crate) edges: Vec<(usize, usize)>,
     pub(crate) edge_planes: Vec<glam::Vec4>,
 }
 
 impl PlanarPolygon {
-    fn calc_eps(n: glam::Vec3, points: &[glam::Vec4]) -> Vec<glam::Vec4> {
+    fn calc_eps(n: glam::Vec3, points: &[glam::Vec4], edges: &[(usize, usize)]) -> Vec<glam::Vec4> {
         let mut edge_planes = Vec::with_capacity(points.len());
-        for i in 0..points.len() {
+        for &(i, j) in edges {
             let a = points[i];
-            let b = points[(i + 1) % points.len()];
+            let b = points[j];
             let edge = b - a;
             let edge_n = edge.xyz().cross(n).normalize();
             edge_planes.push(glam::Vec4::from((edge_n, 0.0)));
@@ -20,14 +21,15 @@ impl PlanarPolygon {
         edge_planes
     }
 
-    pub fn from_points(points: Vec<glam::Vec4>) -> Self {
-        let e1 = points[1] - points[0];
-        let e2 = points[2] - points[1];
+    pub fn from_points_edges(points: Vec<glam::Vec4>, edges: Vec<(usize, usize)>) -> Self {
+        let e1 = points[edges[0].1] - points[edges[0].0];
+        let e2 = points[edges[1].1] - points[edges[1].0];
         let n = e1.xyz().cross(e2.xyz()).normalize();
-        let edge_planes = Self::calc_eps(n, &points);
+        let edge_planes = Self::calc_eps(n, &points, &edges);
         Self {
             pl: glam::Vec4::from((n, -n.dot(points[0].xyz()))),
             points: points.to_vec(),
+            edges,
             edge_planes,
         }
     }
@@ -39,17 +41,19 @@ impl PlanarPolygon {
             glam::Vec4::from((c - u + v, 1.0)),
             glam::Vec4::from((c - u - v, 1.0)),
         ];
-        Self::from_points(points)
+        let edges = vec![(0, 1), (2, 1), (3, 2), (0, 3)];
+        Self::from_points_edges(points, edges)
     }
 
     pub fn with_orientation(&self, trans: glam::Vec3, rot: glam::Mat4) -> Self {
         let out_transform = glam::Mat4::from_translation(trans) * rot;
         let new_n = out_transform * glam::Vec4::from((self.pl.xyz(), 0.0));
         let new_points: Vec<_> = self.points.iter().map(|p| out_transform * p).collect();
-        let new_eps = Self::calc_eps(new_n.xyz(), &new_points);
+        let new_eps = Self::calc_eps(new_n.xyz(), &new_points, &self.edges);
         Self {
             pl: glam::Vec4::from((new_n.xyz(), -new_n.xyz().dot(new_points[0].xyz()))),
             points: new_points,
+            edges: self.edges.clone(),
             edge_planes: new_eps,
         }
     }
--- physics/src/lib.rs
+++ physics/src/lib.rs
@@ -1,4 +1,5 @@
 use glam::Vec4Swizzles;
+use hashbrown::HashMap;
 
 use crate::collision_shape::{
     CollisionShape, Orientation, planar_polygon::PlanarPolygon, sphere::Sphere,
@@ -19,50 +20,88 @@ pub struct Dynamics {
 }
 
 #[derive(Debug, Clone)]
-pub struct RigidBody {
-    name: String,
-    shape: CollisionShape,
-    orientation: Orientation,
-    kinematics: Kinematics,
-    dynamics: Dynamics,
+pub enum Separation {
+    Face {
+        of_first: bool,
+        face_idx: usize,
+    },
+    EdgeCross {
+        idx1: usize,
+        idx2: usize,
+    },
+    EdgeTangent {
+        of_first: bool,
+        edge_idx: usize,
+        normal: glam::Vec3,
+    },
+    PointTangent {
+        of_first: bool,
+        point_idx: usize,
+        normal: glam::Vec3,
+    },
 }
 
-#[derive(Debug, Clone)]
-struct SepPlane {
-    rel_to_first: bool,
-    plane: glam::Vec4,
+impl Separation {
+    pub fn swap_first_object(&mut self) {
+        match self {
+            Separation::Face {
+                of_first,
+                face_idx: _,
+            } => {
+                *of_first = !*of_first;
+            }
+            Separation::EdgeCross { idx1, idx2 } => {
+                (*idx1, *idx2) = (*idx2, *idx1);
+            }
+            Separation::EdgeTangent {
+                of_first,
+                edge_idx: _,
+                normal: _,
+            } => {
+                *of_first = !*of_first;
+            }
+            Separation::PointTangent {
+                of_first,
+                point_idx: _,
+                normal: _,
+            } => {
+                *of_first = !*of_first;
+            }
+        }
+    }
 }
 
-fn sp_sp_sep(s1: &Sphere, s2: &Sphere) -> Option<SepPlane> {
+fn sp_sp_sep(s1: &Sphere, s2: &Sphere) -> Option<Separation> {
     let d = s2.center - s1.center;
     let r = s1.radius + s2.radius;
     if d.dot(d) > r * r {
         let d_dir = d.normalize();
-        let sep_plane = glam::Vec4::from((d_dir, -s1.radius));
-        return Some(SepPlane {
-            rel_to_first: true,
-            plane: sep_plane,
+        return Some(Separation::PointTangent {
+            of_first: true,
+            point_idx: 0,
+            normal: d_dir,
         });
     }
     None
 }
 
-fn sp_ppoly_sep(s: &Sphere, pp: &PlanarPolygon) -> Option<SepPlane> {
+fn sp_ppoly_sep(s: &Sphere, pp: &PlanarPolygon) -> Option<Separation> {
     let pl_c_dist = pp.pl.dot(glam::Vec4::from((s.center, 1.0)));
     if pl_c_dist > s.radius {
-        return Some(SepPlane {
-            rel_to_first: false,
-            plane: pp.pl,
+        return Some(Separation::Face {
+            of_first: false,
+            face_idx: 0,
         });
     }
-    for p in &pp.points {
+    for (i, p) in pp.points.iter().enumerate() {
         let d = p.xyz() - s.center;
         let d_norm = d.normalize();
         let pl = glam::Vec4::from((d_norm, -s.center.dot(d_norm) - s.radius));
         if points_on_pos(pl, &pp.points) {
-            return Some(SepPlane {
-                rel_to_first: false,
-                plane: pl,
+            return Some(Separation::PointTangent {
+                of_first: false,
+                point_idx: i,
+                normal: d_norm,
             });
         }
     }
@@ -76,9 +115,10 @@ fn sp_ppoly_sep(s: &Sphere, pp: &PlanarPolygon) -> Option<SepPlane> {
         let d_norm = d.normalize();
         let pl = glam::Vec4::from((d_norm, -s.center.dot(d_norm) - s.radius));
         if points_on_pos(pl, &pp.points) {
-            return Some(SepPlane {
-                rel_to_first: false,
-                plane: pl,
+            return Some(Separation::EdgeTangent {
+                of_first: false,
+                edge_idx: i,
+                normal: d_norm,
             });
         }
     }
@@ -112,29 +152,31 @@ fn points_on_which_side(pl: glam::Vec4, points: &[glam::Vec4]) -> Option<bool> {
             };
         };
     }
-    None
+    if test_is_pos.is_none() {
+        Some(true)
+    } else {
+        None
+    }
 }
 
-fn ppoly_ppoly_sep(r1: &PlanarPolygon, r2: &PlanarPolygon) -> Option<SepPlane> {
+fn ppoly_ppoly_sep(r1: &PlanarPolygon, r2: &PlanarPolygon) -> Option<Separation> {
     // Check Planes
     if points_on_which_side(r1.pl, &r2.points).is_some() {
-        return Some(SepPlane {
-            rel_to_first: true,
-            plane: r1.pl,
+        return Some(Separation::Face {
+            of_first: true,
+            face_idx: 0,
         });
     }
     if points_on_which_side(r2.pl, &r1.points).is_some() {
-        return Some(SepPlane {
-            rel_to_first: false,
-            plane: r2.pl,
+        return Some(Separation::Face {
+            of_first: false,
+            face_idx: 0,
         });
     }
     // Edge Crosses
-    for i1 in 0..r1.points.len() {
-        let j1 = (i1 + 1) % r1.points.len();
+    for &(i1, j1) in &r1.edges {
         let e1 = r1.points[j1] - r1.points[i1];
-        for i2 in 0..r2.points.len() {
-            let j2 = (i2 + 1) % r2.points.len();
+        for &(i2, j2) in &r2.edges {
             let e2 = r2.points[j2] - r2.points[i2];
             let n = e1.xyz().cross(e2.xyz());
             if n.length_squared() == 0.0 {
@@ -149,32 +191,67 @@ fn ppoly_ppoly_sep(r1: &PlanarPolygon, r2: &PlanarPolygon) -> Option<SepPlane> {
                 continue;
             };
             if side_1 ^ side_2 {
-                let n_norm = n.normalize();
-                return Some(SepPlane {
-                    rel_to_first: true,
-                    plane: glam::Vec4::from((n_norm, -n_norm.dot(r1.points[i1].xyz()))),
-                });
+                return Some(Separation::EdgeCross { idx1: i1, idx2: i2 });
             }
         }
     }
     None
 }
 
-fn coll_shape_sep(a: &CollisionShape, b: &CollisionShape) -> Option<SepPlane> {
+fn coll_shape_sep(a: &CollisionShape, b: &CollisionShape) -> Option<Separation> {
     match a {
         CollisionShape::Sphere(s1) => match b {
             CollisionShape::Sphere(s2) => sp_sp_sep(s1, s2),
             CollisionShape::PlanarPolygon(pp2) => sp_ppoly_sep(s1, pp2),
         },
         CollisionShape::PlanarPolygon(pp1) => match b {
-            CollisionShape::Sphere(s2) => {
-                let mut s_plane = sp_ppoly_sep(s2, pp1);
-                if let Some(sp) = s_plane.as_mut() {
-                    sp.rel_to_first = !sp.rel_to_first;
-                }
-                s_plane
-            }
+            CollisionShape::Sphere(s2) => sp_ppoly_sep(s2, pp1).map(|mut sp| {
+                sp.swap_first_object();
+                sp
+            }),
             CollisionShape::PlanarPolygon(pp2) => ppoly_ppoly_sep(pp1, pp2),
         },
+    }
+}
+
+pub struct RigidBody {
+    pub mass: f32,
+    pub shape: CollisionShape,
+    pub orient: Orientation,
+    pub can_rotate: bool,
+    pub has_gravity: bool,
+    pub dont_interact_mask: u32,
+}
+
+pub struct PhysicsManager {
+    pub objects: Vec<RigidBody>,
+    pub object_ids: HashMap<String, usize>,
+    pub separations: Vec<Vec<Separation>>,
+}
+
+impl PhysicsManager {
+    pub fn new() -> Self {
+        Self {
+            objects: Vec::new(),
+            object_ids: HashMap::new(),
+            separations: Vec::new(),
+        }
+    }
+
+    pub fn add_obj(&mut self, name: &str, obj: RigidBody) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    pub fn forward_ms(&mut self) {
+        for i in 0..self.objects.len() {
+            let mut sliding_surface: Vec<glam::Vec4> = vec![];
+            // Validate separation planes and detect sliding surfaces
+            for j in (i + 1)..self.objects.len() {
+                if (self.objects[i].dont_interact_mask & self.objects[j].dont_interact_mask) != 0 {
+                    continue;
+                }
+            }
+        }
+        todo!()
     }
 }
