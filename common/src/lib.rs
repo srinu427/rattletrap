a17960d2f8e8f63265c8cb0e55510dd48c5f3e70
Srinu427 <srinivasadigopula@gmail.com>
2026-06-28 22:47:12 +0530

    add apply transform when rendering

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,6 @@
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct Entity(u64);
 
@@ -7,8 +10,49 @@ impl Entity {
     }
 }
 
-pub struct Node {
-    pub name: String,
-    pub entity: Entity,
-    pub children: Vec<Self>,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum GeometryInfoDisk {
+    RectCUV {
+        c: [f32; 3],
+        u: [f32; 3],
+        v: [f32; 3],
+    },
+    CubeCUVH {
+        c: [f32; 3],
+        u: [f32; 3],
+        v: [f32; 3],
+        h: f32,
+    },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum MeshInfoDisk {
+    Geo(GeometryInfoDisk),
+    File(String),
+    UsePhysicsShape,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum PhysicsInfoDisk {
+    RigidBody {
+        mass: f32,
+        shape: GeometryInfoDisk,
+        gravity: bool,
+        #[serde(default)]
+        init_velocity: [f32; 3],
+        #[serde(default)]
+        init_acceleration: [f32; 3],
+    },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NodeInfoDisk {
+    name: String,
+    physics: Option<PhysicsInfoDisk>,
+    mesh: Option<MeshInfoDisk>,
+    transform: Option<[[f32; 4]; 4]>,
+}
+
+pub struct GameData {
+    names: IndexMap<Entity, String>,
 }
